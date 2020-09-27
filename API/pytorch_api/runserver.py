220c6695335f0f0197afa0afa6d9998122d69b97
Tyler Estro <testro@cs.stonybrook.edu>
2020-09-27 03:13:26 -0700

    added/tested error reporting. leaving sync for now

--- API/pytorch_api/runserver.py
+++ API/pytorch_api/runserver.py
@@ -10,10 +10,6 @@ import numpy as np
 import uuid
 import sys
 
-
-# defining the api-endpoint  
-#API_ENDPOINT = "https://52.180.95.115:80/v1/pytorch_api/classify"
-
 print("Creating Application")
 
 ACCEPTED_CONTENT_TYPES = ['image/png', 'application/octet-stream', 'image/jpeg']
@@ -39,7 +35,6 @@ model.initialize(model_path)
 
 
 def process_request_data(request):
-	print('Processing data...')
 	return_values = {'image_bytes': None}
 	try:
 		# Attempt to load the body
@@ -54,56 +49,44 @@ def process_request_data(request):
 	api_path = '/classify', 
 	methods = ['POST'], 
 	request_processing_function = process_request_data,
-	maximum_concurrent_requests = 100,
+	maximum_concurrent_requests = 50,
 	content_types = ACCEPTED_CONTENT_TYPES,
-	content_max_length = 100000000, # In bytes
+	content_max_length = 10000000, # In bytes
 	trace_name = 'post:classify')
 
 
 def post(*args, **kwargs):
-	print('Post called')
-	image_bytes = kwargs.get('image_bytes')
-	print('reading image')
-#    ai4e_service.api_task_manager.UpdateTaskStatus(taskId, 'running - segmentating the image')
-#    try:
-	print('reading image')
-	out_im = model.png_predict(image_bytes) 
-	print('done prediction')
-	local_file_name = str(uuid.uuid4()).replace('-','') + '.png'
-	full_path_to_file = './' + local_file_name
-	print(full_path_to_file)
-	print(out_im.shape,out_im.dtype)
-	out_im[out_im>255] = 255
-	Image.fromarray(out_im.astype(np.uint8)).save(full_path_to_file)
-	
-	print(out_im.shape)
+	try:
+		# classifying image
+		image_bytes = kwargs.get('image_bytes')
+		out_im = model.png_predict(image_bytes) 
 
-#    try:
-	# Create the BlockBlockService that is used to call the Blob service for the storage account
-	block_blob_service = BlockBlobService(account_name='icebergblob',
-			account_key='b+sB+qbZvfqR81KThZwor2DjZmkEEo0X1/rpbxUdeIoJUoeIwUSelTnAoULyVtnxdxc8hc2MLMusA0PTFeusuA==')
-	# Create a container called 'penguinapi'.
-	container_name ='penguinapi'
-	block_blob_service.create_container(container_name)
+		local_file_name = str(uuid.uuid4()).replace('-','') + '.png'
+		full_path_to_file = './' + local_file_name
+		out_im[out_im>255] = 255
+		Image.fromarray(out_im.astype(np.uint8)).save(full_path_to_file)
 
-	# Set the permission so the blobs are public.
-	block_blob_service.set_container_acl(container_name, public_access=PublicAccess.Container)
+		# Create the BlockBlockService that is used to call the Blob service for the storage account
+		block_blob_service = BlockBlobService(account_name='icebergblob',
+				account_key='b+sB+qbZvfqR81KThZwor2DjZmkEEo0X1/rpbxUdeIoJUoeIwUSelTnAoULyVtnxdxc8hc2MLMusA0PTFeusuA==')
+		# Create a container called 'penguinapi'.
+		container_name ='penguinapi'
+		block_blob_service.create_container(container_name)
 
-	print('logged to container')
-	# Upload the created file, use local_file_name for the blob name
-	block_blob_service.create_blob_from_path(container_name, local_file_name, full_path_to_file)
-	
-	url = block_blob_service.make_blob_url(container_name, local_file_name)
+		# Set the permission so the blobs are public.
+		block_blob_service.set_container_acl(container_name, public_access=PublicAccess.Container)
 
-	#ai4e_service.api_task_manager.CompleteTask(taskId, 'completed')        
-#    except:
-#        raise IOError('Cannot save file to blob')
+		# Upload the created file, use local_file_name for the blob name
+		block_blob_service.create_blob_from_path(container_name, local_file_name, full_path_to_file)
+		
+		# get the URL of the classified image in our blob
+		url = block_blob_service.make_blob_url(container_name, local_file_name)
 
-	return_dict = {'image_url' : url}
-	return jsonify(return_dict)
- #   except:
- #       log.log_exception(sys.exc_info()[0], taskId)
- #       ai4e_service.api_task_manager.FailTask(taskId, 'failed: ' + str(sys.exc_info()[0]))
+		return_dict = {'image_url' : url}
+		return jsonify(return_dict)
+	except Exception as err:
+		log.log_exception(sys.exc_info()[0])
+		abort(400, '{0}: {1}'.format(type(err).__name__, err))
 
 if __name__ == '__main__':
 	app.run()
